0d3a2172156824c5800f8671f733b4ae1b612ed6
kfishster <kirillvar@gmail.com>
2023-02-20 18:56:35 -0800

    Adding tailwindcss (calf?)

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p className='label'>
-          OnlyCalves official website
+    <div className="flex flex-col justify-center items-center h-screen gap-4">
+      <h1 className="flex text-4xl font-semibold">
+        OnlyCalves official website
+      </h1>
+
+      <div className="group flex flex-col items-center gap-4">
+        <img
+          src="https://thumbs.dreamstime.com/b/fitness-healthy-leg-muscle-leg-muscleillustration-design-vector-isolated-white-background-122386596.jpg"
+          className="flex h-80 w-80 animate-slowspin -z-10"
+          alt="logo"
+        />
+        <p className="label flex text-3xl invisible group-hover:visible">
+          Definition (noun) (ˈkävz): The fleshy back part of the leg below the
+          knee
         </p>
-        <div className="definition">
-          <img src="https://thumbs.dreamstime.com/b/fitness-healthy-leg-muscle-leg-muscleillustration-design-vector-isolated-white-background-122386596.jpg" className="App-logo" alt="logo" />
-          <p className='label'>Definition (noun) (ˈkävz): The fleshy back part of the leg below the knee</p>
-        </div>
-      </header>
+      </div>
     </div>
   );
 }
